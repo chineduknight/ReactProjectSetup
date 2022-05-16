ae8143d1ae71a18c5584fa5b1f3fef9c0aec1579
Chinedu Oguejiofor <chineduknight@gmail.com>
2022-05-16 18:29:35 +0100

    updated the store

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 export const baseURL = process.env.REACT_APP_BASE_URL;
-
 export * from "./request";
 
 const axiosInstance: AxiosInstance = axios.create({
@@ -12,13 +11,32 @@ const axiosInstance: AxiosInstance = axios.create({
   },
 });
 
-//  This adds a token before all the requests.
+// https://dev.to/charlintosh/setting-up-axios-interceptors-react-js-typescript-12k5
+// This adds a token before all the requests.
 // https://stackoverflow.com/questions/57251719/acquiring-a-new-token-with-axios-interceptors
-const addTokenToRequest = (request: AxiosRequestConfig) => {
-  request.headers!.Authorization = `Bearer ${localStorage.getItem("key-to-get")}`;
+const onRequest = (request: AxiosRequestConfig): AxiosRequestConfig => {
+  request.headers!.Authorization = localStorage.getItem("key-here") || "";
   return request;
 };
 
-axiosInstance.interceptors.request.use(addTokenToRequest);
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
+
+const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+  const statusCode = error.response!.status;
+  if (statusCode === 401) {
+    // logout user
+  }
+  return Promise.reject(error);
+};
+
+// https://axios-http.com/docs/interceptors
+axiosInstance.interceptors.request.use(onRequest, onRequestError);
+axiosInstance.interceptors.response.use(onResponse, onResponseError);
 
 export default axiosInstance;
